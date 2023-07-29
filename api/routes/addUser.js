3272f06c4636c7cddb2660ae1a3ccb8eba28adab
Farfa7886 <50551069+Farfa7886@users.noreply.github.com>
2023-07-29 17:16:31 +0200

    validatiopn

--- api/routes/addUser.js
+++ api/routes/addUser.js
@@ -5,7 +5,25 @@ const cache = require('../cache');
 const Users = require('../models/UsersSchema.js');
 
 
-
+function validateUser(username) {
+  if (String(username).length > 30) {
+    return {
+      "success": false,
+      "error": "Maxium username length exceeded (30 characters)"
+    }
+  }
+  else if (isEmpty(username)) {
+    return {
+      "success": false,
+      "error": "Username is empty"
+    }
+  }
+  else {
+    return {
+      "success": true
+    }
+  }
+}
 
 
 router.use(express.json());
@@ -19,17 +37,38 @@ router.post("/add/:username", async (req, res) => {
     })
   }
   else {
-    const user = new Users({"username": req.params.username})
-    user.save().then(() => { 
-      console.log(`Username ${req.params.username} has been added`);
-      cache.get("accounts", (cache.get("accounts").push(req.params.username)))
-      res.status(200).json({
-        "success": true
+    if ((validateUser(req.params.username)).success) {
+      const user = new Users({"username": req.params.username})
+      user.save().then(() => { 
+        console.log(`Username ${req.params.username} has been added`);
+        cache.get("accounts", (cache.get("accounts").push(req.params.username)))
+        res.status(200).json({
+          "success": true
+        })
       })
-    })
+    }
+    else {
+      res.status(500).json(validateUser(req.params.username))
+    }
   }
 
 
 });
+
+function isEmpty(text) {
+  if (text == '' || text == null) return true
+  let splittedText = text.split('');
+  let isEmpty = true;
+  splittedText.forEach(element => {
+      if (isEmpty) {
+          if (element != ' ') {
+              isEmpty = false
+          }
+      }
+  });
+
+  return isEmpty;
+}
+
 
 module.exports = router;
